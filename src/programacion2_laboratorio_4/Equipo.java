3cb0e2af3dd4b33bd5d88eb0d64c860320b2b506
miguelblanco784 <50030229+miguelblanco784@users.noreply.github.com>
2019-05-10 14:39:51 -0600

    Creacion Equipo y Fechas

--- src/programacion2_laboratorio_4/Equipo.java
+++ src/programacion2_laboratorio_4/Equipo.java
@@ -1,33 +1,32 @@
 package programacion2_laboratorio_4;
 
-
-import java.util.ArrayList;
+import java.util.*;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Francisco Pagoaga
  */
 public class Equipo {
+
     private String nombre;
     private String estadio;
     private String pais;
     private String nomentrenador;
     private String nomduenio;
     private String mascota;
-    private String fecha;
+    private Date fecha;
     private String color;
-    private ArrayList<Jugador> jugadores=new ArrayList();
+    private ArrayList<Jugador> jugadores = new ArrayList();
 
     public Equipo() {
     }
 
-    public Equipo(String nombre, String estadio, String pais, String nomentrenador, String nomduenio, String mascota, String fecha, String color) {
+    public Equipo(String nombre, String estadio, String pais, String nomentrenador, String nomduenio, String mascota, Date fecha, String color) {
         this.nombre = nombre;
         this.estadio = estadio;
         this.pais = pais;
@@ -42,73 +41,77 @@ public class Equipo {
         return nombre;
     }
 
-    public String getEstadio() {
-        return estadio;
-    }
-
-    public String getPais() {
-        return pais;
-    }
-
-    public String getNomentrenador() {
-        return nomentrenador;
-    }
-
-    public String getNomduenio() {
-        return nomduenio;
-    }
-
-    public String getMascota() {
-        return mascota;
-    }
-
-    public String getFecha() {
-        return fecha;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public ArrayList<Jugador> getJugadores() {
-        return jugadores;
-    }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getEstadio() {
+        return estadio;
     }
 
     public void setEstadio(String estadio) {
         this.estadio = estadio;
     }
 
+    public String getPais() {
+        return pais;
+    }
+
     public void setPais(String pais) {
         this.pais = pais;
+    }
+
+    public String getNomentrenador() {
+        return nomentrenador;
     }
 
     public void setNomentrenador(String nomentrenador) {
         this.nomentrenador = nomentrenador;
     }
 
+    public String getNomduenio() {
+        return nomduenio;
+    }
+
     public void setNomduenio(String nomduenio) {
         this.nomduenio = nomduenio;
+    }
+
+    public String getMascota() {
+        return mascota;
     }
 
     public void setMascota(String mascota) {
         this.mascota = mascota;
     }
 
-    public void setFecha(String fecha) {
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
         this.fecha = fecha;
+    }
+
+    public String getColor() {
+        return color;
     }
 
     public void setColor(String color) {
         this.color = color;
     }
 
+    public ArrayList<Jugador> getJugadores() {
+        return jugadores;
+    }
+
     public void setJugadores(ArrayList<Jugador> jugadores) {
         this.jugadores = jugadores;
     }
-    
-    
+
+    @Override
+    public String toString() {
+        return "Equipo{" + "nombre=" + nombre + ", estadio=" + estadio + ", pais=" + pais + ", nomentrenador=" + nomentrenador + ", nomduenio=" + nomduenio + ", mascota=" + mascota + ", fecha=" + fecha + ", color=" + color + ", jugadores=" + jugadores + '}';
+    }
+
 }
--- src/programacion2_laboratorio_4/Programacion2_Laboratorio_4.java
+++ src/programacion2_laboratorio_4/Programacion2_Laboratorio_4.java
@@ -1,6 +1,12 @@
 package programacion2_laboratorio_4;
 
+import com.sun.prism.paint.Color;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Programacion2_Laboratorio_4 {
 
@@ -10,9 +16,49 @@ public class Programacion2_Laboratorio_4 {
     public static void main(String[] args) {
         while (true) {
             System.out.print("1. Crear Equipos\n"
-                    + "2. Crear Jugadores\n"
-                    + "3. ");
-        }
+                    + "2. Crear Jugadores para equipo\n"
+                    + "3. Jugar\n"
+                    + "Ingrese su opcion: ");
+            int opcion = leer.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingrese el nombre: ");
+                    String nombre = leer.next();
+                    System.out.print("Ingrese nombre del estadio: ");
+                    String estadio = leer.next();
+                    System.out.print("Ingrese el pais: ");
+                    String pais = leer.next();
+                    System.out.print("Ingrese el nombre del entrenador: ");
+                    String nombreent = leer.next();
+                    System.out.print("Nombre del dueno: ");
+                    String nombreduen = leer.next();
+                    System.out.print("Ingrese la mascota: ");
+                    String mascota = leer.next();
+                    System.out.print("Ingrese la fecha de creacion: \n"
+                            + "Ejemplo yyyy/MM/dd");
+                    String fecha = leer.next();
+                    DateFormat df = new SimpleDateFormat("yyyy/MM/dd");
+                    Date fechacreacion;
+                    try {
+                        fechacreacion = df.parse(fecha);
+                    } catch (ParseException ex) {
+                        System.out.println("La fecha fue ingresada mal no se agregara una fecha");
+                        fechacreacion = new Date();
+                    }
+                    System.out.print("Ingrese el color: ");
+                    String color = leer.next();
+
+                    break;//Fin break de pedir datos
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                default:
+                    throw new AssertionError();
+            }//Fin switch opciones
+        }//Fin while infinito 
     }//Fin main
 
 }//Fin public class
